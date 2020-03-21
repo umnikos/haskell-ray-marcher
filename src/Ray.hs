8ab10e3ef57bba73de730ac5996ed3f1d3895320
umnikos <alex.stefanov2@gmail.com>
2020-03-21 19:04:57 +0200

    Change a few comments

--- src/Ray.hs
+++ src/Ray.hs
@@ -13,8 +13,8 @@ epsilon :: Double
 epsilon = 0.0001
 
 rayMarch :: Scene -> Double -> Ray -> Maybe Color
-rayMarch s end (pos,dir) -- End is the distance where we know we didn't hit anything. (End of scene)
-    | end <= 0 = Nothing -- Means we got to the end of the scene.
-    | dist < 0.0001 = Just $ Vec3 1 0 0 -- Test Color
+rayMarch s end (pos,dir) -- End is the render distance (how far to march before giving up)
+    | end <= 0 = Nothing
+    | dist < 0.0001 = Just $ Vec3 1 0 0 -- Test Color-TODO
     | otherwise = rayMarch s (end-dist) (pos + dist `scale` dir, dir) -- Each time lowering the distance to the end with the distance we traveled.
     where   (dist, _) = s pos
