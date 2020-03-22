0fe7ca1c121ce3fe04c4cceb16a96dc8039f777c
alexvidenov <a.videnov8a@gmail.com>
2020-03-22 15:19:09 +0200

    Exporting basic colors

--- src/Color.hs
+++ src/Color.hs
@@ -1,6 +1,9 @@
 module Color
   ( Color
   , colorize
+  , red
+  , green
+  , blue
   ) where
 
 import Vector
@@ -11,9 +14,10 @@ type Color = Vec3
 colorize :: Color -> Scene -> Scene
 colorize c s pt =
   let (d, (_,p,g)) = s pt -- Evaluating the scene
-  in (d, ( c,p,g )) -- Adding the color to the scene. 
+  in (d, ( c,p,g )) -- Adding the color to the scene.
 
 -- Example Colors
 red, green :: Scene -> Scene
 red = colorize (Vec3 1 0 0)
 green = colorize (Vec3 0 1 0)
+blue = colorize (Vec3 0 0 1)
