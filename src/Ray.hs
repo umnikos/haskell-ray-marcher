9ab7ea9cabb22274a3e92dce687a11c3de75f628
alexvidenov <a.videnov8a@gmail.com>
2020-03-24 15:44:45 +0200

    Added generation of the View Rays.

--- src/Ray.hs
+++ src/Ray.hs
@@ -18,3 +18,21 @@ rayMarch s end (pos,dir) -- End is the render distance (how far to march before 
     | dist < 0.0001 = Just $ Vec3 1 0 0 -- Test Color-TODO
     | otherwise = rayMarch s (end-dist) (pos + dist `scale` dir, dir) -- Each time lowering the distance to the end with the distance we traveled.
     where   (dist, _) = s pos
+
+
+getXComponents :: Int -> [Double]
+getXComponents width = take width [-1, -1 + 2 / fromIntegral width .. ] -- Generates equally spaced floats from -1 to 1.
+
+widthCoords :: [Double]
+widthCoords = getXComponents 1024 -- Width of the image
+
+getYComponents :: Int -> [Double]
+getYComponents height = take height [-1, -1 + 2 / fromIntegral height .. ]
+
+heightCoords :: [Double]
+heightCoords = getYComponents 512 -- Height of the image
+
+getRays :: Double -> [[Ray]]
+getRays fov = map computeRow heightCoords
+    where   z = (tan (fov / 2))
+            computeRow y = [ (Vec3 0 0 0, normalize (Vec3 x (-y) z) ) | x <- widthCoords ] -- First Ray has coordinates [-1,-(-1)].
