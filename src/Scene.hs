94edf2f0164471371ef7c69cda00addcfaebb86b
umnikos <alex.stefanov2@gmail.com>
2020-03-30 15:17:53 +0300

    Add basic documentation for all of the functions

--- src/Scene.hs
+++ src/Scene.hs
@@ -8,15 +8,20 @@ module Scene
   ) where
 
 import Vector
+import Color
 
 type Radius = Double
-type Position = Vec3
+type Position = Vec3 -- ^ A position in 3D space
 type Scene = Position -> (Radius, Material)
-type Material = (Vec3, Double, Double) -- Color, Specular lighting and Gloss(defines how "soft"/"hard" the reflection is)
+type Material = (Vec3 -- Color
+                ,Double -- Specular lighting
+                ,Double) -- Gloss (defines how "soft"/"hard" the reflection is)
 
+-- | Defines a sphere at a given position and with a given radius.
 sphere :: Position -> Radius -> Scene
 sphere pos r = \pt -> (mag (pos-pt) - r, (Vec3 1 1 1, 20, 0.5))
 
+-- | Combines two scenes into a single scene.
 mergeScenes :: Scene -> Scene -> Scene
 mergeScenes scene1 scene2 pt
     | d1 < d2 = res1 -- Picks the minimum distance and the corresponding Material
