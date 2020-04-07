7334b61f287a28856af6c860f9cd0581bc7defcd
umnikos <alex.stefanov2@gmail.com>
2020-04-07 23:32:05 +0300

    Make type aliases correctly depend on each other.

--- src/Marcher.hs
+++ src/Marcher.hs
@@ -20,6 +20,8 @@ module Marcher
   , Radius
   , Position
   , Scene
+  , SpecularLighting
+  , Gloss
   , Material
   , sphere
   , mergeScenes
@@ -32,6 +34,7 @@ module Marcher
 import Codec.Image.PPM ( ColorArray, ppm_p6 )
 import System.IO ( withBinaryFile, hPutStr, IOMode(WriteMode) )
 
+-- | A collection of 3 doubles.
 data Vec3 = Vec3 !Double !Double !Double deriving (Show,Eq)
 
 instance Num Vec3 where
@@ -64,6 +67,7 @@ normalize v = ( 1 / mag v) `scale` v
 
 ------------------------------------------------------------
 
+-- | Color is stored in RGB format.
 type Color = Vec3
 
 -- | Sets the color of an entire scene to some color.
@@ -82,7 +86,8 @@ blue = colorize (Vec3 0 0 1)
 
 ------------------------------------------------------------
 
-type Ray = (Vec3 -- Ray origin
+-- | Stores the ray origin as a position in space and a direction as a normalized vector.
+type Ray = (Position -- Ray origin
            ,Vec3) -- Ray direction
 
 -- | Marches a ray through a scene. Returns Nothing if it goes outside the scene.
@@ -112,12 +117,20 @@ spacedPoints n = f <$> fromIntegral <$> [0..n-1]
 
 ------------------------------------------------------------
 
+-- | Commonly indicates a distance from a given point.
 type Radius = Double
-type Position = Vec3 -- ^ A position in 3D space
+-- | A position in 3D space
+type Position = Vec3
+-- | One or several objects in space.
 type Scene = Position -> (Radius, Material)
-type Material = (Vec3 -- Color
-                ,Double -- Specular lighting
-                ,Double) -- Gloss (defines how "soft"/"hard" the reflection is)
+-- | Specular lighting is the bright spot on shiny objects.
+type SpecularLighting = Double
+-- | Defines how "soft"/"hard" the reflection is.
+type Gloss = Double
+-- | All properties describing an object other than its shape.
+type Material = (Color
+                ,SpecularLighting
+                ,Gloss)
 
 -- | Defines a sphere at a given position and with a given radius.
 sphere :: Position -> Radius -> Scene
