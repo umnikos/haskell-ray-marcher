304b69dd337efb31901684a147c2176149240a53
alexvidenov <a.videnov8a@gmail.com>
2020-04-22 11:33:07 +0300

    Added basic passing unit test for colorizing a scene.

--- test/MarcherSpec/MarcherSpec.hs
+++ test/MarcherSpec/MarcherSpec.hs
@@ -1,10 +1,20 @@
-module MarcherSpec.MarcherSpec (spec) where
+module MarcherSpec.MarcherSpec where
 
 import Test.Hspec
 import Marcher
 
+samplePoint :: Position
+samplePoint = Vec3 (1,2,3)
+
+sampleColor :: Color
+sampleColor = blue
+
+sampleScene :: Scene
+sampleScene = sphere (Vec3 (0, 0, (-3))) 1
+
 spec :: Spec
 spec = do
-  describe "Some spec" $ do
-    it "tests" $ do
-      head [1 ..] `shouldBe` 1
+  describe "Function colorize" $ do
+    it "makes a scene have a particular color" $ do
+      color `shouldBe` blue
+      where (_, (color, _, _)) = colorize sampleColor sampleScene samplePoint
